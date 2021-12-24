f79a17c1da964a3c1fd37c7ecd6eb215db857e77
Jessica Chen <jchen4181@gmail.com>
2021-12-23 20:51:11 -0500

    Setting up react app

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>A whole new app!</h1>
+            </div>
+        );
+    }
+}
